169034ffde680136b9e62b2d682724f6677b0837
Vincent Quatrevieux <vquatrevieux@b2pweb.com>
2025-03-25 09:49:53 +0100

    [FRAM-190] allow to setup all queues using queue:setup --all (#23)
    * feat: allow to setup all queues using queue:setup --all (#FRAM-190)
    
    * ci: run code coverage on PHP 8.0
    
    * ci: remove gearman on coverage tests

--- src/Console/Command/SetupCommand.php
+++ src/Console/Command/SetupCommand.php
@@ -3,6 +3,7 @@
 namespace Bdf\Queue\Console\Command;
 
 use Bdf\Queue\Console\Command\Extension\DestinationExtension;
+use Bdf\Queue\Destination\DestinationInterface;
 use Bdf\Queue\Destination\DestinationManager;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
@@ -44,11 +45,12 @@ class SetupCommand extends Command
      */
     protected function configure(): void
     {
-        $this->configureDestinationOptions($this->getDefinition());
+        $this->configureDestinationOptions($this->getDefinition(), false);
 
         $this
             ->setDescription('Declare or delete a queue from a connection.')
             ->addOption('drop', null, InputOption::VALUE_NONE, 'Delete the queue from connection.')
+            ->addOption('all', null, InputOption::VALUE_NONE, 'Apply on all declared destinations.')
         ;
     }
 
@@ -57,19 +59,21 @@ class SetupCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $destination = $this->createDestination($this->manager, $input);
+        $isDrop = $input->getOption('drop');
 
-        if ($input->getOption('drop')) {
-            $destination->destroy();
+        foreach ($this->destinations($input) as $name => $destination) {
+            if ($isDrop) {
+                $destination->destroy();
 
-            $output->writeln(sprintf('The destination "<info>%s</info>" has been deleted.', $input->getArgument('connection')));
-        } else {
-            $destination->declare();
+                $output->writeln(sprintf('The destination "<info>%s</info>" has been deleted.', $name));
+            } else {
+                $destination->declare();
 
-            $output->writeln(sprintf('The destination "<info>%s</info>" has been declared.', $input->getArgument('connection')));
+                $output->writeln(sprintf('The destination "<info>%s</info>" has been declared.', $name));
+            }
         }
 
-        return 0;
+        return self::SUCCESS;
     }
 
     /**
@@ -78,5 +82,20 @@ class SetupCommand extends Command
     public function complete(CompletionInput $input, CompletionSuggestions $suggestions): void
     {
         $this->createAutocomplete($this->manager, $input, $suggestions);
+    }
+
+    /**
+     * @return iterable<string, DestinationInterface>
+     */
+    private function destinations(InputInterface $input): iterable
+    {
+        if (!$input->getOption('all')) {
+            yield $input->getArgument('connection') => $this->createDestination($this->manager, $input);
+            return;
+        }
+
+        foreach ($this->manager->destinationNames() as $name) {
+            yield $name => $this->manager->create($name);
+        }
     }
 }
